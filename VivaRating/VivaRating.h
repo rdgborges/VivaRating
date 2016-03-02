6e11833bd6c94ac27ff12919f41cdf6a853cfa81
Rodrigo Borges <rborges.soares@gmail.com>
2016-03-02 09:55:13 -0300

    Initial Commit

---
+++ VivaRating/VivaRating.h
@@ -0,0 +1,19 @@
+//
+//  VivaRating.h
+//  VivaRating
+//
+//  Created by Rodrigo Soares on 3/2/16.
+//  Copyright © 2016 Rodrigo Soares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VivaRating.
+FOUNDATION_EXPORT double VivaRatingVersionNumber;
+
+//! Project version string for VivaRating.
+FOUNDATION_EXPORT const unsigned char VivaRatingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VivaRating/PublicHeader.h>
+
+
